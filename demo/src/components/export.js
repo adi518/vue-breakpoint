cb2c3e9dd10e1399891e3810516aaffabafa1da9
Adi Sahar <adisahar1@hotmail.com>
2018-06-18 01:15:31 +0300

    Add automatic-install

--- demo/src/components/export.js
+++ demo/src/components/export.js
@@ -1,3 +1,5 @@
+// https://github.com/Akryum/v-tooltip/blob/master/src/index.js
+
 import VShowAt from './ShowAt'
 import VBreakpoint from './Breakpoint'
 import { Ctor } from './Breakpoint.Ctor'
@@ -9,11 +11,22 @@ export { Model } from './Breakpoint.Model'
 export { default as VShowAt } from './ShowAt'
 export { default as VBreakpoint } from './Breakpoint'
 
-export default { VBreakpoint, VShowAt }
-
 export const Install = {
   install(Vue, config = { breakpoints }) {
     Object.keys(new Ctor({ config }))
       .forEach((name, Component) => Vue.component(name, Component))
   }
 }
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(Install)
+}
+
+export default { VBreakpoint, VShowAt }
