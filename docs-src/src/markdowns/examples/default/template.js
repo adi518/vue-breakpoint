f9e8a8d759755d477bd2bfd73a03ab7d1265553c
Brad Haydon <bradhaydon@gmail.com>
2018-07-17 10:26:30 -0700

    Fix typo in template example

--- docs-src/src/markdowns/examples/default/template.js
+++ docs-src/src/markdowns/examples/default/template.js
@@ -5,7 +5,7 @@ module.exports = `html
       <span v-if="api.isSmall"  style="font-size: 2rem">   😸  </span>
       <span v-if="api.isMedium" style="font-size: 4rem">   😸  </span>
       <span v-if="api.isLarge"  style="font-size: 6rem">   😸  </span>
-      <span v-if="api.isXLarge" style="font-size: 8rem">   😸  </span>
+      <span v-if="api.isXlarge" style="font-size: 8rem">   😸  </span>
       <span v-if="api.noMatch"  style="font-size: 10rem">  😸  </span>
     </div>
   </v-breakpoint>
