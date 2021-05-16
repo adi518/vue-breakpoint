b693428163f612d1f15bc47dc2979ba51b81c888
adi518 <adi.s@openweb.com>
2021-05-17 00:29:25 +0300

    fix babel.config.js; update deps

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-const { resolveExternals } = require('node-externals')
+const { resolveExternals } = require('monorepo-utils')
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // https://stackoverflow.com/a/45820235/4106263
     config.externals(resolveExternals({ importType: 'umd' }))
   },
   pluginOptions: {
     jestSerializer: {
-      removeComments: true
-    }
-  }
+      removeComments: true,
+    },
+  },
 }
